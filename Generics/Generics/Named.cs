c6ff554f6aa205f8d83222e51cd19e5772b787c0
alexalex972 <33548921+alexalex972@users.noreply.github.com>
2018-01-06 23:42:06 +0200

    generics practical task solution

---
+++ Generics/Generics/Named.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Named
+{
+    public class Named<T>
+    {
+        public T value { get; set; }
+        public string name { get; set; }
+    }
+    public class Variable<T> : Named<int>
+    {
+
+        public static void swapValue(Variable<T> left, Variable<T> right) // oprator = cannot be overloaded
+        { // операция =, която присвоява стойността на една променлива на друга без да променя името
+            left.value = right.value;
+        }
+
+        public static void showValue(Variable<T> i)
+        // едноместна операция *, която връща стойността на променливата
+        {
+
+            Console.WriteLine(i.value);
+        }
+
+
+        // операции == и !=, които сравняват две променливи
+        public static bool operator ==(Variable<T> left, Variable<T> right)
+        {
+            if (left.value == right.value && left.name == right.name)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        public static bool operator !=(Variable<T> left, Variable<T> right)
+        {
+            if (left.value == right.value && left.name == right.name)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+
+    // Клас за extension methods, в който да се имплементират extension методи
+    // за задаване на стойността на променливата и извеждането и на конзолата.
+
+    public static class Extension
+    {
+
+        public static void setValue(this Variable<int> var, int x)
+        {
+
+            var.value = x;
+
+        }
+
+        public static void getValue(this Variable<int> var)
+        {
+
+            Console.WriteLine(var.value);
+
+        }
+
+    }
+
+}
---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Named
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var a = new Variable<int>();
+            a.value = 6;
+            a.name = "myname";
+
+            var b = new Variable<int>();
+            b.value = 5;
+            b.name = "myname";
+
+            Variable<int>.showValue(a);
+            Variable<int>.showValue(b);
+            Console.WriteLine(a == b);
+            Console.WriteLine(a != b);
+
+            Variable<int>.swapValue(a, b);
+            Variable<int>.showValue(a);
+            Variable<int>.showValue(b);
+            Console.ReadLine();
+
+            b.getValue();
+            b.setValue(10);
+            b.getValue();
+
+            Console.ReadLine();
+        }
+    }
+}
