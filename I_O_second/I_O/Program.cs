97e4167e6f85065984b5e09751b8efd0374b873f
alexalex972 <33548921+alexalex972@users.noreply.github.com>
2018-01-20 00:18:14 +0200

    I/O practical task different solution

---
+++ I_O_second/I_O/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace I_O
+{
+    class Program
+    {
+        
+
+        public static void requieredSubjects(List<Subject> a, int subjectWanted)
+        {
+            List<int> requiered = new List<int>();
+            
+
+            Console.WriteLine("Desired Subject: " + a[subjectWanted].subject);
+            Console.WriteLine();
+            if (a[subjectWanted].requierments.Count == 0)
+            {
+                Console.WriteLine("Requiered Subjects None");
+            }
+            else
+            {
+                foreach(Subject item in a[subjectWanted].requierments)
+                {
+                    requiered.Add(item.subject);
+                    
+                    item.printReq(requiered);
+                    
+                }
+
+                Console.WriteLine("Requiered Subject:");
+                List<int> noDupes = requiered.Distinct().ToList();
+                foreach (int item in noDupes)
+                {
+                    Console.WriteLine(item);
+                }
+
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            List<Subject> collectionSubject = new List<Subject>();
+
+            var logFile = File.ReadAllLines("requierments.txt");
+            var logList = new List<string>(logFile);
+            string[] subjects;
+            
+
+            for(int i=0; i <logList.Count;i++)
+            {
+               subjects = logList[i].Split(' ');
+               Subject sub = new Subject();
+               sub.subject = Convert.ToInt32(subjects[0]);
+
+                
+
+               collectionSubject.Add(sub); 
+
+
+            }
+
+            for (int j = 0; j < logList.Count; j++)
+            {
+                subjects = logList[j].Split(' ');
+                
+
+
+                if (collectionSubject[j].subject == Convert.ToInt32(subjects[0]))
+                {
+                    foreach(string item in subjects.Skip(1))
+                    {
+                        if (collectionSubject[j].subject == Convert.ToInt32(item))
+                        {
+                            Console.WriteLine("Error, Please fix it! ");
+                            Console.ReadLine();
+                            return;
+                        }
+                        else { collectionSubject[j].addReq(item, collectionSubject); }
+                    }
+                }
+                
+
+            }
+
+            requieredSubjects(collectionSubject, 3);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ I_O_second/I_O/Subject.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I_O
+{
+    public class Subject
+    {
+        public int subject;
+        public List<Subject> requierments = new List<Subject>();
+
+        public List<int> printReq(List<int> requie)
+        {
+
+           
+            foreach (Subject a in requierments)
+            {
+           
+                requie.Add(a.subject);
+            }
+
+            return requie;
+
+        }
+        
+
+       
+
+        public void addReq(string b, List<Subject> s)
+        {
+           
+            
+                foreach(Subject el in s)
+                {
+                    if(Convert.ToInt32(b) == el.subject)
+                    {
+                        requierments.Add(el);
+                    }
+                
+            }
+            
+        }
+
+
+
+    }
+}
