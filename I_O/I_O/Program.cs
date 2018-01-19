508b3b810d893cf7b56bfd144f947ec7e41861d6
alexalex972 <33548921+alexalex972@users.noreply.github.com>
2018-01-19 14:10:47 +0200

    I/O practical task solution

---
+++ I_O/I_O/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace I_O
+{
+    class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+
+            List<Subject> collectionSubject = new List<Subject>();
+
+            var logFile = File.ReadAllLines("requierments.txt");
+            var logList = new List<string>(logFile);
+            string[] subjects;
+
+            foreach(string item in logList)
+            {
+               subjects = item.Split(' ');
+               Subject sub = new Subject();
+               sub.subject = subjects[0];
+                foreach (string req in subjects.Skip(1))
+                {
+                    sub.requierments.Add(req);
+                }
+
+                collectionSubject.Add(sub); 
+
+            }
+
+
+            foreach(Subject a in collectionSubject)
+            {
+                Console.WriteLine(a.subject);
+                Console.WriteLine("- - -");
+                a.printReq();
+                Console.WriteLine();
+            }
+            
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ I_O/I_O/Subject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I_O
+{
+    public class Subject
+    {
+        public string subject = "";
+        public List<string> requierments = new List<string>();
+
+        public void printReq()
+        {
+            foreach(string a in requierments)
+            {
+                Console.WriteLine(a);
+            }
+        }
+        
+    }
+}
