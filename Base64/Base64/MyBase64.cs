8bbd47e1a0a7c69b5725d31bf2a96d598ae392c2
alexalex972 <33548921+alexalex972@users.noreply.github.com>
2018-01-21 11:23:01 +0200

    Base64 practical task solution

---
+++ Base64/Base64/MyBase64.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base64
+{
+    class MyBase64
+    {
+        public string encodedString;
+
+        public MyBase64(char[] initial)
+        {
+            byte[] endocedByte = ASCIIEncoding.ASCII.GetBytes(initial);
+            encodedString = System.Convert.ToBase64String(endocedByte);
+
+        }
+
+    }
+}
---
+++ Base64/Base64/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base64
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] b = new char[] { };
+
+            b = Console.ReadLine().ToCharArray();
+            MyBase64 a = new MyBase64(b);
+
+            Console.WriteLine(a.encodedString);
+            Console.ReadLine();
+            
+        }
+    }
+}
